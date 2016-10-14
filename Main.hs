ba912458d2ebe8a6237d6401fbc269ce35498145
Grancalavera <leoncoto@gmail.com>
2016-10-14 20:41:21 +0000

    Add line ends fixing functionality

--- Main.hs
+++ Main.hs
@@ -3,12 +3,23 @@ import System.Environment (getArgs)
 main = do 
   args <- getArgs
   case args of
-    [inp,out] -> interctWith fixlines inp out
+    [inp,out] -> interactWith fixLines inp out
     _         -> putStrLn "error: exactly 2 args required"
     
-interctWith f inpf outf = do
+interactWith f inpf outf = do
   input <- readFile inpf
   writeFile outf (f input)
   
-fixlines :: String -> String
-fixlines s = s
+fixLines :: String -> String
+fixLines cs = unlines (splitLines cs)
+
+splitLines :: String -> [String]
+splitLines [] = []
+splitLines cs = let (pre,suf) = break isLineTerminator cs
+  in pre : case suf of
+    ('\r':'\n':rest) -> splitLines rest
+    ('\r':rest)      -> splitLines rest
+    ('\n':rest)      -> splitLines rest
+    _                -> []
+
+isLineTerminator c = c == '\r' || c == '\n'
